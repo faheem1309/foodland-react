17448fcfbbbc6d4f4398faca2fca8da3fb015f3f
faheem1309 <masfmohammed027@gmail.com>
2026-01-27 15:30:11 +0530

    updates

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,16 +1,22 @@
+import React from "react";
 import "../styles/Navbar.css";
+import logo from "../assets/images/logo.png";
 
-function Navbar() {
+const Navbar = () => {
   return (
     <nav className="navbar">
-      <h1>FoodLand 🍽️</h1>
-      <ul>
+      <div className="nav-left">
+        <img src={logo} alt="FoodLand Logo" className="logo" />
+        <span className="brand-name">FoodLand</span>
+      </div>
+
+      <ul className="nav-links">
         <li>Home</li>
         <li>Recipes</li>
         <li>Contact</li>
       </ul>
     </nav>
   );
-}
+};
 
 export default Navbar;
